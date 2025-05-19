6fdbd525367ac4c7b55f261d8b16b1f481bf4ab0
Timon <yurikikadet@gmail.com>
2025-05-19 16:42:33 +0600

    переделанная в 100-й раз

---
+++ DynamicIntArray.h
@@ -0,0 +1,65 @@
+
+#ifndef DINAMYCINTARRAY_H
+#define DINAMYCINTARRAY_H
+#include <iostream>
+
+class DynamicIntArray {
+private:
+    int _size;
+    int _buffer;
+    int *_array;
+public:
+    // rule of 6
+    DynamicIntArray();
+
+    explicit DynamicIntArray(int);
+
+    DynamicIntArray(int, int);
+
+    DynamicIntArray(const DynamicIntArray &);
+
+    DynamicIntArray &operator=(const DynamicIntArray &);
+
+    DynamicIntArray(DynamicIntArray &&);
+
+    DynamicIntArray &operator=(DynamicIntArray &&);
+
+    ~DynamicIntArray();
+
+    int getSize() const;
+
+    int &operator[](int);
+
+    int operator[](int) const;
+
+    void resize(int);
+
+    bool operator==(const DynamicIntArray &) const;
+
+    bool operator!=(const DynamicIntArray &) const;
+
+    bool operator<(const DynamicIntArray &) const;
+
+    bool operator<=(const DynamicIntArray &) const;
+
+    bool operator>(const DynamicIntArray &) const;
+
+    bool operator>=(const DynamicIntArray &) const;
+
+    friend DynamicIntArray operator+(const DynamicIntArray &, const DynamicIntArray &);
+
+    friend std::ostream &operator<<(std::ostream &, const DynamicIntArray &);
+
+    friend std::istream &operator>>(std::istream &, DynamicIntArray &);
+
+    void reserve(int);
+
+    int capacity() const;
+
+    void pushBack(int);
+
+    int popBack();
+
+    DynamicIntArray(int, int, int);
+};
+#endif //DINAMYCINTARRAY_H
